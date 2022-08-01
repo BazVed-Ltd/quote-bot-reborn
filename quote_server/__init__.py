3b359172ed10802b86cff540a9be52e3de8fe28c
Evgeniy Rybin <z.ribin20@gmail.com>
2022-08-01 16:39:14 +0500

    Тут не нужно

--- quote_server/__init__.py
+++
@@ -1,5 +0,0 @@
-from aiohttp import web
-from quote_server.routes import routes
-
-app = web.Application()
-app.add_routes(routes)
--- quote_server/__main__.py
+++
@@ -1,5 +0,0 @@
-from aiohttp import web
-
-from quote_server import app
-
-web.run_app(app)
