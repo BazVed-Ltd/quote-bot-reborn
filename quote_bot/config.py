65514d9df653fd46b6d8f4074929bc59429c1a82
platofff <42435266+platofff@users.noreply.github.com>
2022-08-04 16:48:48 +0300

    Настраиваемый log level, по умолчанию INFO

--- quote_bot/config.py
+++ quote_bot/config.py
@@ -1,4 +1,6 @@
 from os import getenv
+import logging
+
 from dotenv import load_dotenv
 
 def load_config():
@@ -7,4 +9,5 @@ def load_config():
     config["vk_token"] = getenv("VK_TOKEN")
     config["mongodb_uri"] = getenv("MONGODB_URI") or "mongodb://mongodb:mongodb@mongodb:27017"
     config["mongodb_db_name"] = getenv("MONGODB_DB_NAME") or "quote_dev"
+    logging.basicConfig(level=getattr(logging, getenv('LOG_LEVEL') or 'INFO'))
     return config
